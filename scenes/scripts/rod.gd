476d177b18172f8f4bc14a1133e4619faf8cb8fe
Justin Crouch <jcrouch938@gmail.com>
2024-03-07 17:47:05 +0100

    Core rod mechanic finished
    Changed logic to state-based control

--- scenes/scripts/rod.gd
+++ scenes/scripts/rod.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @onready var INTERACT = $Interact
+@onready var PANEL = $Panel
 @onready var DISPLAY_TIMER = $DisplayTimer
 @onready var TIMER = $Timer
 @onready var COOLDOWN = $Cooldown
@@ -8,32 +9,66 @@ extends Area2D
 @export_range(1.0, 10.0) var MIN_TIME = 1.0
 @export_range(1.0, 10.0) var MAX_TIME = 1.0
 
-var active = true
+enum STATES {
+	WAIT,
+	WAITING,
+	TICKING,
+	TICK,
+	DISABLED
+}
+@export var state: STATES
+var nearby = false
 
 func _ready():
-	COOLDOWN.wait_time = randf_range(MIN_TIME, MAX_TIME)
-	COOLDOWN.start()
+	state = STATES.WAIT
 
 
 func _process(delta):
-	DISPLAY_TIMER.text = str( snapped(TIMER.time_left, .1) )
+	match(state):
+		STATES.WAIT:
+			TIMER.stop()
+			COOLDOWN.wait_time = randf_range(MIN_TIME, MAX_TIME)
+			COOLDOWN.start()
+			
+			INTERACT.visible = false
+			DISPLAY_TIMER.visible = false
+			PANEL.modulate = Color(0, 255, 255)
+			
+			state = STATES.WAITING
+			
+		STATES.WAITING:
+			pass
+			
+		STATES.TICK:
+			TIMER.start()
+			DISPLAY_TIMER.visible = true
+			state = STATES.TICKING
+			
+		STATES.TICKING:
+			DISPLAY_TIMER.text = str( snapped(TIMER.time_left, .1) )
+			
+			if(nearby): INTERACT.visible = true
+			else: 		INTERACT.visible = false
+			
+			if(nearby && Input.is_action_pressed("INTERACT")): state = STATES.WAIT
+			
+		STATES.DISABLED:
+			INTERACT.visible = false
+			DISPLAY_TIMER.visible = false
+			PANEL.modulate = Color(255, 0, 0)
+			
+
+func _on_cooldown_timeout():
+	state = STATES.TICK
+
+func _on_timer_timeout():
+	state = STATES.DISABLED
 
 
 func _on_body_entered(body):
-	if(body.name != 'Player' && active): return
-	INTERACT.visible = true
-	
+	if(body.name != 'Player'): return
+	nearby = true
 
 func _on_body_exited(body):
-	if(body.name != 'Player' && active): return
-	INTERACT.visible = false
-
-
-func _on_timer_timeout():
-	active = false
-	INTERACT.visible = false
-	DISPLAY_TIMER.visible = false
-
-
-func _on_cooldown_timeout():
-	TIMER.start()
+	if(body.name != 'Player'): return
+	nearby = false
